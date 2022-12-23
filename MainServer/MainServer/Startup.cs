e73948f7f1636bc077a9ddb3d3209b526cb89694
Code-Nostra <artem2002228228@mail.ru>
2022-12-23 15:52:58 +0300

    StableVersion

--- MainServer/MainServer/Startup.cs
+++ MainServer/MainServer/Startup.cs
@@ -33,7 +33,7 @@ namespace MainServer
         {
             IdentityModelEventSource.ShowPII = true;
             services.AddEntityFrameworkSqlite().AddDbContext<DB>();
-            //
+            ////
             //services.AddLogging(
             //builder =>
             //{
